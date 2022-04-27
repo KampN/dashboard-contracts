a75bee804bc1b929f67585e56060a1768f44bd5b
vincent <vincent.falduto@gmail.Com>
2022-04-27 14:30:57 +0200

    Add QueryConstant.php

---
+++ Contract/Constant/QueryConstant.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Kampn\Dashboard\Contract\Constant;
+
+class QueryConstant {
+	public const RESOURCE = 'resource';
+	public const SEGMENTS = 'segments';
+	public const OPERATION_NAME = 'operation_name';
+	public const START_DATE = 'start_date';
+	public const END_DATE = 'end_date';
+	public const FILTERS = 'filters';
+}
