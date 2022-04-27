4b65c164c022fcf558c68e38235f4f7f840031b6
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2022-04-27 12:03:52 +0200

    add money field

--- Contract/Enum/FieldTypeEnum.php
+++ Contract/Enum/FieldTypeEnum.php
@@ -5,6 +5,7 @@ namespace Kampn\Dashboard\Contract\Enum;
 class FieldTypeEnum {
 
     public const NUMERIC = 'numeric';
+    public const MONEY = 'money';
     public const TEXT = 'text';
     public const DATETIME = 'datetime';
 
