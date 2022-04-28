0b4564fe8c969a2c643d8e7a35cb6c6a666a50c0
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2022-04-28 13:14:57 +0200

    add field constants to express campaign resource

--- Contract/Interfaces/ResourceInterface.php
+++ Contract/Interfaces/ResourceInterface.php
@@ -6,12 +6,6 @@ use Kampn\Dashboard\Service\Query\Query;
 
 interface ResourceInterface {
 
-	public const FIELD_NAME = 'name';
-	public const FIELD_DESCRIPTION = 'description';
-	public const FIELD_IS_SEGMENT = 'is_segment';
-	public const FIELD_TYPE = 'type';
-	public const FIELD_SELECTABLE = 'selectable';
-	public const FIELD_OPERATORS = 'operators';
 
 	public const SERVICE_TYPE_ROWS = 'rows';
 	public const SERVICE_TYPE_TOTAL = 'total';
