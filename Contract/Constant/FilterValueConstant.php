e5d15dead741fd96c62384268e9f5e52ac44f59d
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2022-05-17 10:22:44 +0200

    add filter value constant

---
+++ Contract/Constant/FilterValueConstant.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Kampn\Dashboard\Contract\Constant;
+
+class FilterValueConstant {
+	public const STATUS_ACTIVE = 'active';
+	public const STATUS_PAUSED = 'paused';
+	public const STATUS_REMOVED = 'removed';
+}
