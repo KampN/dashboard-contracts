e47d20f51ddf27bba0bad755738792345ed099cf
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2023-02-13 17:08:27 +0100

    fix validator unit tests

--- tests/Service/Query/ValidatorTest.php
+++ tests/Service/Query/ValidatorTest.php
@@ -2,6 +2,8 @@
 
 namespace Kampn\Dashboard\Tests\Service\Query;
 
+use DateTime;
+use DateTimeInterface;
 use Kampn\Dashboard\Contract\Constant\FilterConstant;
 use Kampn\Dashboard\Contract\Constant\QueryConstant;
 use Kampn\Dashboard\Contract\Enum\OperatorEnum;
@@ -12,103 +14,166 @@ use Kampn\Dashboard\Service\Query\Validator;
 use Kampn\Dashboard\Tests\Configuration\Stub\AdCampaignResourceServiceStub;
 use PHPUnit\Framework\TestCase;
 
-class ValidatorTest extends TestCase {
-	public function subject(): Validator {
-		return new Validator();
-	}
+class ValidatorTest extends TestCase
+{
+    public function testValidate() : void
+    {
+        $resource = new AdCampaignResourceServiceStub();
+        $query    = Query::build([
+            QueryConstant::RESOURCE   => AdCampaignResourceServiceStub::class,
+            QueryConstant::SEGMENTS   => ['date'],
+            QueryConstant::START_DATE => (new DateTime())->format(DateTimeInterface::ATOM),
+            QueryConstant::END_DATE => (new DateTime())->modify('+30d')->format(DateTimeInterface::ATOM),
+            QueryConstant::FILTERS    => FiltersEncoder::encode([
+                [
+                    FilterConstant::OPERAND  => 'cost',
+                    FilterConstant::OPERATOR => OperatorEnum::GREATER_THAN,
+                    FilterConstant::VALUES   => [0],
+                ],
+            ]),
+        ]);
 
-	public function testValidate(): void {
-		$resource = new AdCampaignResourceServiceStub();
-		$query = Query::build([
-			QueryConstant::RESOURCE => AdCampaignResourceServiceStub::class,
-			QueryConstant::SEGMENTS => ['date'],
-			QueryConstant::FILTERS => FiltersEncoder::encode([
-				[
-					FilterConstant::OPERAND => 'cost',
-					FilterConstant::OPERATOR => OperatorEnum::GREATER_THAN,
-					FilterConstant::VALUES => [0]
-				]
-			]),
-		]);
+        $this->subject()->validate($resource, $query);
+        $this->assertTrue(true);
+    }
 
-		$this->subject()->validate($resource, $query);
-		$this->assertTrue(true);
-	}
+    public function subject() : Validator
+    {
+        return new Validator();
+    }
 
-	public function testValidateWhenSegmentInvalid(): void {
-		$resource = new AdCampaignResourceServiceStub();
-		$query = Query::build([
-			QueryConstant::RESOURCE => AdCampaignResourceServiceStub::class,
-			QueryConstant::SEGMENTS => ['riri', 'fifi', 'loulou']
-		]);
+    public function testValidateWhenSegmentInvalid() : void
+    {
+        $resource = new AdCampaignResourceServiceStub();
+        $query    = Query::build([
+            QueryConstant::RESOURCE => AdCampaignResourceServiceStub::class,
+            QueryConstant::SEGMENTS => ['riri', 'fifi', 'loulou'],
+            QueryConstant::START_DATE => (new DateTime())->format(DateTimeInterface::ATOM),
+            QueryConstant::END_DATE => (new DateTime())->modify('+30d')->format(DateTimeInterface::ATOM),
+        ]);
 
-		$this->expectException(QueryException::class);
-		$this->expectExceptionMessage('Missing segment riri in resource');
+        $this->expectException(QueryException::class);
+        $this->expectExceptionMessage('Missing segment riri in resource');
 
-		$this->subject()->validate($resource, $query);
-		$this->assertTrue(true);
-	}
+        $this->subject()->validate($resource, $query);
+        $this->assertTrue(true);
+    }
 
-	public function testValidateWhenFilterOperandInvalid(): void {
-		$resource = new AdCampaignResourceServiceStub();
-		$query = Query::build([
-			QueryConstant::RESOURCE => AdCampaignResourceServiceStub::class,
-			QueryConstant::SEGMENTS => ['date'],
-			QueryConstant::FILTERS => FiltersEncoder::encode([
-				[
-					FilterConstant::OPERAND => 'yolo',
-					FilterConstant::OPERATOR => OperatorEnum::LOWER_THAN,
-					FilterConstant::VALUES => [0]
-				]
-			]),
-		]);
+    public function testValidateWhenFilterOperandInvalid() : void
+    {
+        $resource = new AdCampaignResourceServiceStub();
+        $query    = Query::build([
+            QueryConstant::RESOURCE => AdCampaignResourceServiceStub::class,
+            QueryConstant::SEGMENTS => ['date'],
+            QueryConstant::START_DATE => (new DateTime())->format(DateTimeInterface::ATOM),
+            QueryConstant::END_DATE => (new DateTime())->modify('+30d')->format(DateTimeInterface::ATOM),
+            QueryConstant::FILTERS  => FiltersEncoder::encode([
+                [
+                    FilterConstant::OPERAND  => 'yolo',
+                    FilterConstant::OPERATOR => OperatorEnum::LOWER_THAN,
+                    FilterConstant::VALUES   => [0],
+                ],
+            ]),
+        ]);
 
-		$this->expectException(QueryException::class);
-		$this->expectExceptionMessage('Invalid field yolo in filter');
+        $this->expectException(QueryException::class);
+        $this->expectExceptionMessage('Invalid field yolo in filter');
 
-		$this->subject()->validate($resource, $query);
-		$this->assertTrue(true);
-	}
+        $this->subject()->validate($resource, $query);
+        $this->assertTrue(true);
+    }
 
-	public function testValidateWhenFilterOperatorInvalid(): void {
-		$resource = new AdCampaignResourceServiceStub();
-		$query = Query::build([
-			QueryConstant::RESOURCE => AdCampaignResourceServiceStub::class,
-			QueryConstant::SEGMENTS => ['date'],
-			QueryConstant::FILTERS => FiltersEncoder::encode([
-				[
-					FilterConstant::OPERAND => 'cost',
-					FilterConstant::OPERATOR => OperatorEnum::NOT_LIKE,
-					FilterConstant::VALUES => [0]
-				]
-			]),
-		]);
+    public function testValidateWhenFilterOperatorInvalid() : void
+    {
+        $resource = new AdCampaignResourceServiceStub();
+        $query    = Query::build([
+            QueryConstant::RESOURCE => AdCampaignResourceServiceStub::class,
+            QueryConstant::SEGMENTS => ['date'],
+            QueryConstant::START_DATE => (new DateTime())->format(DateTimeInterface::ATOM),
+            QueryConstant::END_DATE => (new DateTime())->modify('+30d')->format(DateTimeInterface::ATOM),
+            QueryConstant::FILTERS  => FiltersEncoder::encode([
+                [
+                    FilterConstant::OPERAND  => 'cost',
+                    FilterConstant::OPERATOR => OperatorEnum::NOT_LIKE,
+                    FilterConstant::VALUES   => [0],
+                ],
+            ]),
+        ]);
 
-		$this->expectException(QueryException::class);
-		$this->expectExceptionMessage('Invalid operator like for field cost in filter');
+        $this->expectException(QueryException::class);
+        $this->expectExceptionMessage('Invalid operator like for field cost in filter');
 
-		$this->subject()->validate($resource, $query);
-		$this->assertTrue(true);
-	}
+        $this->subject()->validate($resource, $query);
+        $this->assertTrue(true);
+    }
 
-	public function testValidateWhenFilterValuesInvalid(): void {
-		$resource = new AdCampaignResourceServiceStub();
-		$query = Query::build([
-			QueryConstant::RESOURCE => AdCampaignResourceServiceStub::class,
-			QueryConstant::SEGMENTS => ['date'],
-			QueryConstant::FILTERS => FiltersEncoder::encode([
-				[
-					FilterConstant::OPERAND => 'cost',
-					FilterConstant::OPERATOR => OperatorEnum::GREATER_THAN,
-					FilterConstant::VALUES => 0
-				]
-			]),
-		]);
+    public function testValidateWhenFilterValuesInvalid() : void
+    {
+        $resource = new AdCampaignResourceServiceStub();
+        $query    = Query::build([
+            QueryConstant::RESOURCE => AdCampaignResourceServiceStub::class,
+            QueryConstant::SEGMENTS => ['date'],
+            QueryConstant::START_DATE => (new DateTime())->format(DateTimeInterface::ATOM),
+            QueryConstant::END_DATE => (new DateTime())->modify('+30d')->format(DateTimeInterface::ATOM),
+            QueryConstant::FILTERS  => FiltersEncoder::encode([
+                [
+                    FilterConstant::OPERAND  => 'cost',
+                    FilterConstant::OPERATOR => OperatorEnum::GREATER_THAN,
+                    FilterConstant::VALUES   => 0,
+                ],
+            ]),
+        ]);
 
-		$this->expectException(QueryException::class);
-		$this->expectExceptionMessage('Invalid values for field cost in filter');
+        $this->expectException(QueryException::class);
+        $this->expectExceptionMessage('Invalid values for field cost in filter');
 
-		$this->subject()->validate($resource, $query);
-		$this->assertTrue(true);
-	}
+        $this->subject()->validate($resource, $query);
+        $this->assertTrue(true);
+    }
+
+    public function testValidateWhenMissingStartDate() : void
+    {
+        $resource = new AdCampaignResourceServiceStub();
+        $query    = Query::build([
+            QueryConstant::RESOURCE => AdCampaignResourceServiceStub::class,
+            QueryConstant::SEGMENTS => ['date'],
+            QueryConstant::END_DATE => (new DateTime())->modify('+30d')->format(DateTimeInterface::ATOM),
+            QueryConstant::FILTERS  => FiltersEncoder::encode([
+                [
+                    FilterConstant::OPERAND  => 'cost',
+                    FilterConstant::OPERATOR => OperatorEnum::GREATER_THAN,
+                    FilterConstant::VALUES   => 0,
+                ],
+            ]),
+        ]);
+
+        $this->expectException(QueryException::class);
+        $this->expectExceptionMessage('Missing start date in query');
+
+        $this->subject()->validate($resource, $query);
+        $this->assertTrue(true);
+    }
+
+    public function testValidateWhenMissingEndDate() : void
+    {
+        $resource = new AdCampaignResourceServiceStub();
+        $query    = Query::build([
+            QueryConstant::RESOURCE => AdCampaignResourceServiceStub::class,
+            QueryConstant::SEGMENTS => ['date'],
+            QueryConstant::START_DATE => (new DateTime())->format(DateTimeInterface::ATOM),
+            QueryConstant::FILTERS  => FiltersEncoder::encode([
+                [
+                    FilterConstant::OPERAND  => 'cost',
+                    FilterConstant::OPERATOR => OperatorEnum::GREATER_THAN,
+                    FilterConstant::VALUES   => 0,
+                ],
+            ]),
+        ]);
+
+        $this->expectException(QueryException::class);
+        $this->expectExceptionMessage('Missing end date in query');
+
+        $this->subject()->validate($resource, $query);
+        $this->assertTrue(true);
+    }
 }
