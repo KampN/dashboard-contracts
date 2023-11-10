2f0ae23338585d34e3f2e6ab67d8f0c69ce115d1
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2023-11-10 10:47:34 +0100

    typo

--- Contract/Resource/ExpressCampaignResource.php
+++ Contract/Resource/ExpressCampaignResource.php
@@ -88,7 +88,7 @@ interface ExpressCampaignResource extends ResourceInterface
         [
             ResourceFieldConstant::FIELD_NAME        => self::FIELD_FRANCHISE_LABEL,
             ResourceFieldConstant::FIELD_DESCRIPTION => 'Express franchise label',
-            ResourceFieldConstant::FIELD_IS_SEGMENT  => true,
+            ResourceFieldConstant::FIELD_IS_SEGMENT  => false,
             ResourceFieldConstant::FIELD_TYPE        => FieldTypeEnum::TEXT,
             ResourceFieldConstant::FIELD_SELECTABLE  => true,
             ResourceFieldConstant::FIELD_OPERATORS   => ResourceFieldConstant::DEFAULT_TYPE_OPERATORS[FieldTypeEnum::TEXT],
@@ -96,7 +96,7 @@ interface ExpressCampaignResource extends ResourceInterface
         [
             ResourceFieldConstant::FIELD_NAME        => self::FIELD_FRANCHISE_STATUS,
             ResourceFieldConstant::FIELD_DESCRIPTION => 'Express franchise status',
-            ResourceFieldConstant::FIELD_IS_SEGMENT  => true,
+            ResourceFieldConstant::FIELD_IS_SEGMENT  => false,
             ResourceFieldConstant::FIELD_TYPE        => FieldTypeEnum::TEXT,
             ResourceFieldConstant::FIELD_SELECTABLE  => true,
             ResourceFieldConstant::FIELD_OPERATORS   => ResourceFieldConstant::DEFAULT_TYPE_OPERATORS[FieldTypeEnum::TEXT],
